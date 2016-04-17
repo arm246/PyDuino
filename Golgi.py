0b2f92041b76c5e53153b92aa89310407efd3496
arm246 <arm246.cornell.edu>
2016-04-17 16:50:36 -0400

    updated Golgi for message send/receive testing

--- Golgi.py
+++ Golgi.py
@@ -1,1 +1,22 @@
-import 
+import PySerial
+
+to = 5
+bytecount = 32
+
+ser =   serial.Serial('COM4', 9600,
+            parity=serial.PARITY_NONE,
+            stopbits=serial.STOPBITS_ONE,
+            bytesize=serial.EIGHTBITS,
+            timeout = to) # Establish the connection on a specific port
+
+class Sensor:
+    
+    def __init__(self, name, arduino,pin):
+        self.name = name
+        self.arduino = arduino
+        self.pin = pin
+        
+    def read(self ):
+        ser.write(request)
+        sleep(.1)
+        ser.read(bytecount)
